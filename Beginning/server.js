29d2d1d1b97d0a23ba7619cddfabd00877af80c5
Tanvir Ahmed <tanvir007v@gmail.com>
2023-09-05 01:14:37 -0500

    play with nodejs tutorials

---
+++ Beginning/server.js
@@ -0,0 +1,21 @@
+// console.log(global);
+const { add, subtract, multiply, divide } = require("./math");
+// const os = require("os");
+// const path = require("path");
+
+console.log(add(4, 2));
+console.log(subtract(4, 2));
+console.log(multiply(4, 2));
+console.log(divide(4, 2));
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// console.log(path.dirname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
+
+// console.log(path.parse(__filename));
